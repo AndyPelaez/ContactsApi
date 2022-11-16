cddbdcf749479b87f0dcc6c1bda7ce8094d81246
Andy Pelaez <andy.pelaez@ntsprint.com>
2022-11-16 11:05:43 -0500

    fixed phone number validation

--- src/validators/phone-number-validator.ts
+++ src/validators/phone-number-validator.ts
@@ -1,4 +1,4 @@
 export function phoneNumberValidator(value: string) {
-  const numberRegexp = / \d+ /;
-  return value.length < 12 && numberRegexp.test(value);
+  const numberRegexp = /^\+?[0-9]+$/;
+  return value.length < 15 && numberRegexp.test(value);
 }
